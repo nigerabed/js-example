e50da753f22b553176237dfb0086e301c0d7e420
abedsujan <abedsujan@gmail.com>
2023-11-09 20:56:40 +0100

    exlain loop

---
+++ loop/loop.js
@@ -0,0 +1,21 @@
+let arrayData = ["a", "b", "c"];
+
+for(let i=0; i<arrayData.length; i++){
+    if(arrayData[i].name =="fullname"){
+
+    }
+}
+
+arrayData.forEach(function (element){
+    if(element.name =="fullname"){
+
+    }
+});
+
+
+
+arrayData.forEach(element => {
+    if(element.name =="fullname"){
+
+    }
+});
